4b7daaffdf9d289f7a9a9432cec0697a9a1c1db2
ylee <ylee@roblox.com>
2023-02-08 16:13:38 -0800

    add options for data, conda

--- data.sh
+++ data.sh
@@ -8,10 +8,19 @@ dname=kinetics400
 dgz=k400models.tar.gz
 drepo=Video-Swin-Transformer
 
+while getopts d:c: flag
+do
+    case "${flag}" in
+        d) odata=${OPTARG};;
+        c) oconda=${OPTARG};;
+    esac
+done
+
 if false; then
 aws s3 ls
 fi
 
+if [ "$odata" == "y" ]; then
 echo prepare validation dataset and models
 [[ -e "$dpath" ]] || mkdir -p $dpath
 aws s3 cp s3://$dname/$dgz $dpath 
@@ -21,16 +30,22 @@ rm $dpath/$dgz
 echo link dataset
 [[ -e "$dswin/$drepo/data" ]] || ln -s $dpath $dswin/$drepo/data
 ls -al $dswin/$drepo/data
+fi
 
+if [ "$oconda" == "y" ]; then
 if [[ "${CONDA_DEFAULT_ENV}" != "mmlab" ]]; then   # "${CONDA_PREFIX##*/}"
 	# source $dconda/anaconda3/bin/activate mmlab
 	source ${CONDA_EXE%/*}/activate mmlab
 	echo changed to conda env: ${CONDA_DEFAULT_ENV}
 fi
-echo current conda env: "${CONDA_DEFAULT_ENV}"
+fi
 
+echo current conda env: "${CONDA_DEFAULT_ENV}"
 pushd $dswin/$drepo
-python setup.py develop
+
+ommact=`echo $(conda list | grep mmaction2 | wc -l) | sed -e 's/^[[:space:]]*//'`
+echo $ommact
+[[ "$ommact" != "1" ]] && python setup.py develop
 
 for mdl in tiny small base; do
 	[[ "$mdl" == "base" ]] && trsz="1k 22k" || trsz="1k"
@@ -51,4 +66,4 @@ python tools/train.py \
 	--cfg-options load_from=./data/kinetics400/models/swin_${mdl}_patch244_window877_kinetics400_${tr}.pth
 popd
 
-conda deactivate
+${CONDA_EXE} deactivate
