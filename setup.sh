6e67cce91d5aa3e80ec7d96b3cc171c57ad4ee59
ylee <ylee@roblox.com>
2023-02-21 04:34:35 +0000

    editing setup

--- setup.sh
+++ setup.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-
-if [ $# -ge 1 ] && [ "$1" == "mlp" ]; then
-	echo MLP
-	nvidia-smi
-	
-	echo MLP get-key
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B53DC80D13EDEF05
-
-	echo "MLP- libGL1"
-	sudo apt-get update && sudo apt-get install libgl1
-fi
-
-if [ $# -ge 1 ] && [ "$1" == "m60" ]; then
-	vcuda=10.2.89
-	vtorch=1.8.1+cu102
-	vvision=0.9.1+cu102
-else
-	vcuda=11.0.3
-	vtorch=1.7.0+cu110
-	vvision=0.8.1+cu110
-fi
-
-ilevel=0  # 0: 
-while getopts i: flag
+# usage: bash setup.sh mlp -i 2  # setup all
+device=mlp
+taws=0 && tconda=0
+task=1
+while getopts t:m:c: flag
 do
-    case "${flag}" in
-	    i) ilevel=$((10#${OPTARG}));;
-    esac
+  case "${flag}" in
+    t) task=${OPTARG};;    # decimal int
+    m) device=${OPTARG};;    # mlp | m60 | t4 | a10
+    c) tconda=2;;    # decimal int
+  esac
 done
-echo install level: $ilevel
+echo install level: $ilevel, device: $device
 
+if [ $device == "mlp" ]; then
+  echo MLP
+  nvidia-smi
 
-if [ $ilevel -ge 2 ]; then
+  echo MLP get-key
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B53DC80D13EDEF05
+
+  echo "MLP- libGL1"
+  sudo apt-get update && sudo apt-get install libgl1
+fi
+
+if [ $device == "m60" ]; then
+  vcuda=10.2.89 && vtorch=1.8.1+cu102 && vvision=0.9.1+cu102
+else
+  vcuda=11.0.3  && vtorch=1.7.0+cu110 && vvision=0.8.1+cu110
+fi
+
+if [ $taws -ge $task ]; then
 echo s3fs + awscli
 sudo apt update && sudo apt upgrade -y
 sudo apt install s3fs
@@ -39,13 +38,15 @@ pip install --upgrade pip
 pip install awscli
 fi
 
-if [ $ilevel -ge 2 ]; then
+if [ $tconda -ge $task -a -z ${CONDA_EXE+x} ]; then
 echo conda install
 f=Anaconda3-2022.10-Linux-x86_64.sh
 [[ -e $f ]] || wget https://repo.anaconda.com/archive/$f
 bash $f  # NOT sudo
 rm $f
 fi
+exit 1
+
 
 if [ $ilevel -ge 0 ]; then
 echo conda_exe: ${CONDA_EXE%/*}
@@ -91,3 +92,4 @@ python setup.py develop
 popd
 popd
 fi
+
